ad35bfe7c2b1e4799d2ccba1b3d1d979bd268312
elijahred23 <elijahred23@gmail.com>
2023-08-31 23:34:27 +0000

    Eli-2: Added EliGPT application with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
+import ChatApp from './ChatApp';
 import axios from 'axios'; // Import the axios library
 
 function App() {
 	return (
 		<>
-			<h1>Hello World</h1>
+			<ChatApp />
 		</>
 	);
 }
---
+++ src/ChatApp.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './ChatApp.css';
+
+const ChatApp = () => {
+	const [chatHistory, setChatHistory] = useState([]);
+	const [userInput, setUserInput] = useState('');
+	const [isLoading, setIsLoading] = useState(false);
+
+	const apiKey = '';
+
+	const generateResponse = async (userInput) => {
+		const apiUrl = 'https://api.openai.com/v1/chat/completions';
+		const messages = [{ role: 'user', content: userInput }];
+
+		const response = await fetch(apiUrl, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${apiKey}`,
+			},
+			body: JSON.stringify({
+				messages,
+				model: 'gpt-3.5-turbo',
+			}),
+		});
+
+		const responseData = await response.json();
+		const chatGPTResponse = responseData.choices[0].message.content;
+		console.log(chatGPTResponse);
+		return chatGPTResponse;
+	};
+
+	const handleUserInput = async () => {
+		if (userInput.trim() === '') return;
+
+		setIsLoading(true);
+		const response = await generateResponse(userInput);
+		setChatHistory([
+			...chatHistory,
+			{ sender: 'You', message: userInput },
+			{ sender: 'EliGPT', message: response },
+		]);
+		setUserInput('');
+		setIsLoading(false);
+	};
+
+	const handleKeyPress = (event) => {
+		if (event.key === 'Enter') {
+			handleUserInput();
+		}
+	};
+
+	return (
+		<div className="chat-container">
+			<h1 className="app-title">EliGPT</h1>
+			<div className="chat-history">
+				{chatHistory.map((message, index) => (
+					<div key={index} className={`message ${message.sender.toLowerCase()}`}>
+						<span className="sender">{message.sender}: </span>
+						<ReactMarkdown className="message-text">{message.message}</ReactMarkdown>
+
+					</div>
+				))}
+			</div>
+			<div className="user-input">
+				<input
+					type="text"
+					placeholder="Type your message..."
+					value={userInput}
+					onChange={(e) => setUserInput(e.target.value)}
+					onKeyPress={handleKeyPress}
+				/>
+				<button onClick={handleUserInput} disabled={isLoading}>
+					{isLoading ? 'Loading...' : 'Send'}
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ChatApp;
