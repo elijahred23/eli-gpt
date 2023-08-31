6707d039a5970b3f5d78371eca6d346f3bceb9e8
elijahred23 <elijahred23@gmail.com>
2023-08-31 22:55:56 +0000

    Eli-1: Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios'; // Import the axios library
+
+function App() {
+	return (
+		<>
+			<h1>Hello World</h1>
+		</>
+	);
+}
+
+export default App;
